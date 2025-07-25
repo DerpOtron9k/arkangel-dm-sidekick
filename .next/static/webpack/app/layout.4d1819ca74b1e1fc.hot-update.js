5eac5028f89d98f9f3f1f733ac457d0c34fc1e21
DerpOtron9k <staheli.andrew.g@gmail.com>
2025-07-25 12:12:14 -0500

    Add .next/static/webpack/app/layout.4d1819ca74b1e1fc.hot-update.js 1753463534030

---
+++ .next/static/webpack/app/layout.4d1819ca74b1e1fc.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/layout",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fproject%2Fsandbox%2Fuser-workspace%2Fsrc%2Fapp%2Fglobals.css&modules=%2Fproject%2Fsandbox%2Fuser-workspace%2Fsrc%2Fcomponents%2FSidebar.tsx&server=false!":
+/*!**************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fproject%2Fsandbox%2Fuser-workspace%2Fsrc%2Fapp%2Fglobals.css&modules=%2Fproject%2Fsandbox%2Fuser-workspace%2Fsrc%2Fcomponents%2FSidebar.tsx&server=false! ***!
+  \**************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/app/globals.css */ \"(app-client)/./src/app/globals.css\"));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/components/Sidebar.tsx */ \"(app-client)/./src/components/Sidebar.tsx\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRnByb2plY3QlMkZzYW5kYm94JTJGdXNlci13b3Jrc3BhY2UlMkZzcmMlMkZhcHAlMkZnbG9iYWxzLmNzcyZtb2R1bGVzPSUyRnByb2plY3QlMkZzYW5kYm94JTJGdXNlci13b3Jrc3BhY2UlMkZzcmMlMkZjb21wb25lbnRzJTJGU2lkZWJhci50c3gmc2VydmVyPWZhbHNlIS5qcyIsIm1hcHBpbmdzIjoiQUFBQSw2SkFBd0Y7QUFDeEYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz80ZjE4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiL3Byb2plY3Qvc2FuZGJveC91c2VyLXdvcmtzcGFjZS9zcmMvYXBwL2dsb2JhbHMuY3NzXCIpO1xuaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCIvcHJvamVjdC9zYW5kYm94L3VzZXItd29ya3NwYWNlL3NyYy9jb21wb25lbnRzL1NpZGViYXIudHN4XCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fproject%2Fsandbox%2Fuser-workspace%2Fsrc%2Fapp%2Fglobals.css&modules=%2Fproject%2Fsandbox%2Fuser-workspace%2Fsrc%2Fcomponents%2FSidebar.tsx&server=false!\n"));
+
+/***/ })
+
+});
